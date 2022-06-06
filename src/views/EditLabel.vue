0cef4fe2a3039b56c8fa3fd738b28c77d6124080
Wristwang <2117678336@qq.com>
2022-06-06 10:38:49 +0800

    添加左箭头

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,22 +1,24 @@
 <template>
     <layout >
-        编辑标签
+        <div>
+            <Icon name='left'/>
+            <span>编辑标签</span>
+            </div>
+           
     </layout>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import tagListModel from "@/models/tagListModel";
+
 @Component
 export default class EditLabel extends Vue {
     created(){
         const id = this.$route.params.id;
         tagListModel.fetch();
-        
         const tags = tagListModel.data;
-      
         const tag =tags.find(t=>t.id===id);
-        
         if(tag){
             console.log(tag);
         }else{
